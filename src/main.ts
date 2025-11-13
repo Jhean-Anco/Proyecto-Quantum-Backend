ae8d26d35ae8a185b74c7fa098ed1a1702139306
Jhean Pier <73259563@continental.edu.pe>
2025-11-13 10:56:33 -0500

    Prueba Conexion Frontend

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,16 @@ import 'reflect-metadata';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: false });
 
-  // ✅ Habilitar CORS correctamente
   app.enableCors({
-    origin: 'http://localhost:4200', // o ['http://localhost:4200'] si prefieres lista
+    origin: 'https://quantum-proyect.grupo-digital-nextri.com/',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'], // 👈 muy importante para POST JSON
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
     preflightContinue: false,
     optionsSuccessStatus: 204,
   });
 
-  const port = process.env.PORT ?? 3000;
+  const port = 3000;
   await app.listen(port);
-  console.log(`🚀 Servidor NestJS corriendo en http://localhost:${port}`);
 }
 bootstrap();
